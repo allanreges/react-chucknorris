2c380094e67936767cab38d8f06bc483a0c21be6
Allan de Carvalho <allan.reges@fbiz.com.br>
2020-03-25 11:04:16 -0300

    Updates

---
+++ precache-manifest.1b57964a9612528103454deb0a44ae86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130da8790eedc441d04ae6217ee60a34",
+    "url": "/react-chucknorris/index.html"
+  },
+  {
+    "revision": "4ca96f99b3a8b1777928",
+    "url": "/react-chucknorris/static/js/2.77614b1a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-chucknorris/static/js/2.77614b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86e63c0e00a3ebcabbc",
+    "url": "/react-chucknorris/static/js/main.e2db983f.chunk.js"
+  },
+  {
+    "revision": "8320aefe2642fa8c112f",
+    "url": "/react-chucknorris/static/js/runtime-main.ae44cada.js"
+  },
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/react-chucknorris/static/media/chucknorris_logo_coloured_small@2x.39b94c0e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.123e435f3e4381347c392af880ae90e3.js"
+  "/react-chucknorris/precache-manifest.1b57964a9612528103454deb0a44ae86.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-chucknorris/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
