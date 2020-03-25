2adb8d6a2b4c389bf4a9a7e14e4b3955455131d9
Allan de Carvalho <allan.reges@fbiz.com.br>
2020-03-25 01:48:52 -0300

    Updates

---
+++ precache-manifest.3785452ee6457c349f713e630a323252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6d2891658dbfedcbc0b372647edba4",
+    "url": "/react-chucknorris/index.html"
+  },
+  {
+    "revision": "4ca96f99b3a8b1777928",
+    "url": "/react-chucknorris/static/js/2.77614b1a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-chucknorris/static/js/2.77614b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a049559531e6db2494",
+    "url": "/react-chucknorris/static/js/main.626bf2ca.chunk.js"
+  },
+  {
+    "revision": "8320aefe2642fa8c112f",
+    "url": "/react-chucknorris/static/js/runtime-main.ae44cada.js"
+  },
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/react-chucknorris/static/media/chucknorris_logo_coloured_small@2x.39b94c0e.png"
+  }
+]);
